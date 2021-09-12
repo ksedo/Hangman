68662087f2f4cd2b1e62543c212c9abfccbf8c63
EmmettBrown28 <79405919+EmmettBrown28@users.noreply.github.com>
2021-09-12 15:02:01 +0700

    Renderer files

---
+++ src/draw.h
@@ -0,0 +1,17 @@
+#ifndef DRAW_H
+#define DRAW_H
+
+#include <iostream>
+#include <locale.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+using namespace std;
+
+extern int misse;
+extern int missm;
+extern int missh;
+
+void hangman_draw(int misstakes, int difficulty);
+
+#endif
